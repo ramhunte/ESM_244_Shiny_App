6273c763c083c16ae67c449efc6c0b4a27b16d54
zrennie <zoerennie@ucsb.edu>
2023-02-20 17:12:07 -0800

    draft app

---
+++ zoe_version_app.R
@@ -0,0 +1,41 @@
+library(tidyverse)
+library(httr)
+library(jsonlite)
+library(shiny)
+library(shinydashboard)
+
+path <- "https://api.eia.gov/v2/co2-emissions/co2-emissions-aggregates/data/?frequency=annual&data[0]=value&sort[0][column]=period&sort[0][direction]=desc&offset=0&length=5000&api_key=mdOP5PhYCWMt6IrK7c2qcDCslb7MrPpyNahheLlF"
+raw_data <- GET(path)
+emissions_data <- fromJSON(rawToChar(raw_data$content), flatten = TRUE)
+df_emissions <- emissions_data$response$data
+df_emissions %>% filter(stateId=="CA", fuelId == "CO")
+
+
+# Define UI for application that draws a histogram
+ui <- dashboardPage(
+  dashboardHeader(title  = "Energy Usage and Greenhouse Gas Emissions", titleWidth=450),
+    dashboardSidebar(width = 500,
+                     sidebarMenu(id = "sidebarid",
+                      style = "position:fixed; width:auto; overflow-x: clip;",
+                      menuItem("Dashboard", tabName="dashboard"),
+                      menuItem("Energy Usage", tabName="energy"),
+                      menuItem("Greenhouse Gas Emissions", tabname="emissions",
+                      menuSubItem("Sub-item 1", tabName="subitem1"),
+                      menuSubItem("Sub-item 2", tabName = "subitem2"))
+                      )),
+                      dashboardBody()
+  )
+
+   
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    # output$energyPlot <- renderPlot({
+    #     # generate bins based on input$bins from ui.R
+    #   
+    # })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
