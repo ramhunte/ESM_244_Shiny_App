d74ebe002b931678e6f00719d5e66408ba3a0bfb
zrennie <zoerennie@ucsb.edu>
2023-02-27 22:32:13 -0800

    update to map and widgets

--- volt_app_draft.R
+++ volt_app_draft.R
@@ -1,28 +1,22 @@
-library(tidyverse)
-library(httr)
-library(jsonlite)
 library(shiny)
 library(shinydashboard)
-library(knitr)
-library(leaflet)
 library(sf)
+library(dplyr)
+library(janitor)
+library(here)
+library(leaflet)
+library(tigris)
 
-path <- "https://api.eia.gov/v2/co2-emissions/co2-emissions-aggregates/data/?frequency=annual&data[0]=value&sort[0][column]=period&sort[0][direction]=desc&offset=0&length=5000&api_key=mdOP5PhYCWMt6IrK7c2qcDCslb7MrPpyNahheLlF"
-raw_data <- GET(path)
-emissions_data <- fromJSON(rawToChar(raw_data$content), flatten = TRUE)
-df_emissions <- emissions_data$response$data
-df_emissions %>% filter(stateId=="CA", fuelId == "CO")
-
-
-# Define UI for application that draws a histogram
+# Define UI for application
 ui <- dashboardPage(
   dashboardHeader(title  = "Energy Usage and Greenhouse Gas Emissions", titleWidth=450),
   dashboardSidebar(width = 500,
                    sidebarMenu(id = "sidebarid",
                                style = "position:fixed; width:auto; overflow-x: clip;",
                                menuItem("Dashboard", tabName="dashboard"),
-                               selectizeInput("states", "State Selection:",
-                                              choices = state.name, multiple=T),
+                               selectInput("years", label="Select year", choices = 1970:2020, selected = 2020),
+                               # selectizeInput("states", "State Selection:",
+                               #                choices = state.name, multiple=T),
                                menuItem("Energy Usage", tabName="energy",
                                         sliderInput("number", "Number:",
                                                     min = 0, max = 1000,
@@ -38,21 +32,46 @@ ui <- dashboardPage(
                                         menuSubItem("Sub-item 1", tabName="subitem1"),
                                         menuSubItem("Sub-item 2", tabName = "subitem2"))
                    )),
-  dashboardBody(leafletOutput("USMAP"))
-  
-)
+  dashboardBody(
+   fluidRow(
+     tabBox(
+       title = "Emissions Maps",
+      tabPanel("Total Emissions", leafletOutput("total_emissions")),
+      tabPanel("Emissions Per Capita", "content")
+      ))
+   )
+  )
 
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+  #read_sf(here("cb_2021_us_all_500k", "cb_2021_us_state_500k", "cb_2021_us_state_500k.shp"))
+  st <- states() %>% st_transform('+proj=longlat +datum=WGS84')
+  US <-  st %>%
+    clean_names() %>%
+    mutate(state_name=name)
+ # states_3857 <- st_transform(states, 3857)
+  states_emissions <- inner_join(US, emissions_all_fuels, by="state_name")
   
-states <- read_sf("cb_2018_us_state_500k.shp")
   
-  output$USMAP <- renderLeaflet({
-   leaflet(states) %>% 
-     addTiles() %>%
-      addPolygons()
+  output$total_emissions <- renderLeaflet({
+    date_emissions <-  states_emissions %>% subset(period == input$years)
+    pal <- colorNumeric("YlOrRd", date_emissions$value)
+   leaflet() %>%
+      addTiles() %>%
+     
+      addPolygons(data = date_emissions,
+                  fillColor = ~pal(value),
+                  weight = 0.5,
+                  fillOpacity = 0.5,
+                  smoothFactor = 0.2,
+                  label = ~value,
+                  stroke = T, color = "black") %>%
+     addLegend("bottomright", pal = pal, values = date_emissions$value,
+               title = "Carbon emissions", labFormat = labelFormat(suffix = "mmt")) %>%
+      setView(lng = -96.25, lat = 39.50, zoom = 4)
   })
+  
 }
 
 # Run the application 
